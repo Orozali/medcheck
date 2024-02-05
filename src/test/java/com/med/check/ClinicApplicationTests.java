e875eae9ddb53e49670465adcaf3716995c4fd8b
Apple <apple@Apples-MacBook-Pro.local>
2024-02-05 01:16:03 -0600

    First commit

---
+++ src/test/java/com/med/check/ClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.med.check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
