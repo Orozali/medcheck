4a7b8e60cace7b4a612b01342db15afbeddecbc1
Apple <apple@Apples-MacBook-Pro.local>
2024-02-22 01:32:53 -0600

    Added some changes

--- src/main/java/com/med/check/db/api/AuthenticationApi.java
+++ src/main/java/com/med/check/db/api/AuthenticationApi.java
@@ -39,7 +39,7 @@ public class AuthenticationApi {
 
     @Operation(summary = "Forget password method", description = "This method is for sending user url to change his password!")
     @PostMapping("/forget-password")
-    public SimpleResponse forgetPassword(@RequestBody ForgetPasswordRequest request){
+    public SimpleResponse forgetPassword(@RequestBody @Valid ForgetPasswordRequest request){
         return authenticationService.sendEmail(request);
     }
 
@@ -54,5 +54,4 @@ public class AuthenticationApi {
     public AuthenticationResponse resetPassword(@RequestParam String email, @RequestBody @Valid ResetPasswordRequest request){
         return authenticationService.resetPassword(request.newPassword(), email);
     }
-
 }
